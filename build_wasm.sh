f7d68f4a6ac29c6b1f16763d78f6012f18924e19
sky <sky_8691@qq.com>
2025-04-06 13:49:44 +0800

    .

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./init.sh
+rm -f go.mod go.sum
+go mod init minebuster
+go mod tidy
 mkdir -p ./output
 
 # 编译Linux版本
--- init.sh
+++
@@ -1,3 +0,0 @@
-rm -f go.mod go.sum
-go mod init minebuster
-go mod tidy
